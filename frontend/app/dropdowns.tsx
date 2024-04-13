09084eef58f8b094da152e8a4a4eb7329af35be5
juanrios456 <103297471+juanrios456@users.noreply.github.com>
2024-04-13 11:21:08 -0500

    Add files via upload

--- frontend/app/dropdowns.tsx
+++ frontend/app/dropdowns.tsx
@@ -1,27 +1,169 @@
 'use client'
-import { Select, SelectItem } from "@nextui-org/react"
+import { Button, Input, Link, Select, SelectItem,Table, TableHeader,TableColumn,TableBody,TableRow,TableCell } from "@nextui-org/react"
+import { useState } from "react";
 
-type dummyArray = {
+export type dummyArray = {
   name: string;
-  
+}[];
 
+export type InputedDrinks = {
+  name: string;
+  caffeine: number;
+  quantity: number;
 }[]
 
-export function DropDowns(
-  {dummyArray}:{
-    dummyArray: dummyArray;
+const startingFormFields = {
+    name: undefined,
+    size: undefined,
+    quantity: undefined,
+  };
+
+const dummyArray = [
+  {
+    name: 'Monster Energy',
+    caffeine: 11.25,
+    fluidOunce: 16,
+  },
+  {
+    name: 'Bang',
+    caffeine: 5.25,
+    fluidOunce: 8,
   }
-){
+]
+
+const drinksListDummyData:InputedDrinks = [
+  {
+    name: 'Monster',
+    caffeine: 8,
+    quantity: 4,
+  },
+  {
+    name: 'Bang',
+    caffeine: 5.25,
+    quantity: 2,
+  }
+]
+
+export function Dropdowns(){
+  const [formState, setFormState] = useState(startingFormFields);
+  const { name, size, quantity } = formState;
+
+  const handleChange = ({target}:any)=>{
+    setFormState((prev) =>({
+      ...prev,
+      [target.name]: target.value
+    }))
+  }
+
+  const handleSubmit = async () => {
+    try {
+      const response = await fetch('http://127.0.0.1:8080/api/home',{
+        method:'POST',
+        body: JSON.stringify(formState),
+      })
+
+      console.log(formState);
+    } catch (error) {
+      
+    }
+  }
+
+  const handleReset = ()=>{
+    setFormState(startingFormFields);
+  }
+
   return(
-    <div>
-      <div>
-      <Select placeholder="hello">
+  <div>
+    <div className="flex flex-row flex-wrap space-x-2">
+      <div className="min-w-[300px]">
+      <Select placeholder="Select Drink" name="name" aria-label="hello" value={name} onChange={handleChange}>
         {dummyArray.map((item,index)=>(
           <SelectItem key={item.name}>{item.name}</SelectItem>
         ))}
       </Select>
       </div>
-      <div></div>
+      <div>
+        <Input type='number' value={size} aria-label='hi' name="size" placeholder="Fl. Oz." onChange={handleChange}/>
+      </div>
+      <div>
+        <Input type='number' name="quantity" aria-label="hello" value={quantity} placeholder="Quantity" onChange={handleChange}/>
+      </div>
+      <div className="space-x-2">
+        <Button onClick={handleSubmit} >Submit</Button>
+        <Button onClick={handleReset} >Clear</Button>
+      </div>
+    </div>
+    <div className="py-4 flex justify-between">
+      <ProgressBar/>
+      <DrinkList inputedDrinks={drinksListDummyData} />
+    </div>
+  </div>
+  )
+}
+
+
+export function DrinkList({inputedDrinks}:{inputedDrinks:InputedDrinks}){
+  return(
+    <div>
+    <Table aria-label="Example static collection table">
+      <TableHeader>
+        <TableColumn>#</TableColumn>
+        <TableColumn>NAME</TableColumn>
+        <TableColumn>Quantity</TableColumn>
+        <TableColumn>Caffeine (mg)</TableColumn>
+      </TableHeader>
+      <TableBody>
+        {
+          inputedDrinks.map((item,index:number)=>(
+            <TableRow key={item.name}>
+              <TableCell>{index+1}</TableCell>
+              <TableCell>{item.name}</TableCell>
+              <TableCell>{item.quantity} </TableCell>
+              <TableCell>{item.caffeine}</TableCell>
+            </TableRow>
+          ))
+        }
+      </TableBody>
+    </Table>
+    </div>
+  )
+}
+
+export function ProgressBar(){
+  const [filled, isFilled] = useState(0);
+  const [isRunning,setIsRunning] = useState(false);
+
+  return(
+    <div className="flex">
+      <div className="flex items-end border w-[200px] h-[400px] rounded-lg">
+        <div className="w-full h-[40px] bg-black rounded-lg"> </div>
+      </div>
+      <div className="h-[400px] grid grid-cols-1 content-stretch pl-2 ">
+        <span className="relative h-full">400
+          <span className='absolute top-0 left-[-8px] w-[20px] border'></span>
+          <span className='absolute bottom-[75px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[50px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[25px] left-[-8px] w-[10px] border'></span>
+        </span>
+        <span className="relative h-full">300
+          <span className='absolute top-0 left-[-8px] w-[20px] border'></span>
+          <span className='absolute bottom-[75px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[50px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[25px] left-[-8px] w-[10px] border'></span>
+        </span>
+        <span className="relative h-full">200
+          <span className='absolute top-0 left-[-8px] w-[20px] border'></span>
+          <span className='absolute bottom-[75px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[50px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[25px] left-[-8px] w-[10px] border'></span>
+        </span>
+        <span className="relative h-full">100
+          <span className='absolute top-0 left-[-8px] w-[20px] border'></span>
+          <span className='absolute bottom-[75px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[50px] left-[-8px] w-[10px] border'></span>
+          <span className='absolute bottom-[25px] left-[-8px] w-[10px] border'></span>
+        </span>
+      </div>
     </div>
   )
 }
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,32 +1,24 @@
 import Image from "next/image"; 
 import { GiCoffeeCup } from "react-icons/gi";
-import { DropDowns } from "./dropdowns";
+import { Dropdowns } from "./dropdowns";
 
-const dummyArray = [
-  {
-    name: 'Monster Energy',
-    caffeine: 11.25,
-    fluidOunce: 16,
-  },
-  {
-    name: 'Bang',
-    caffeine: 5.25,
-    fluidOunce: 8,
-  }
-]
 
 const hi = "hi";
 
 export default function Home() {
+
+
+
   return (
-    <main className=" flex justify-center">
-      <div className="w-full max-w-[1200px]">
+    <main className=" flex justify-center bg-slate-500 min-h-[100vh]">
+      <div className="w-full max-w-[1000px]">
         <div className="flex items-center justify-center my-4">
           <GiCoffeeCup size='200px'/>
         </div>
-        <div>
-          <DropDowns dummyArray={dummyArray}/>
+        <div className="flex justify-center">
+          <Dropdowns />
         </div>
+        
       </div>
     </main>
   );
